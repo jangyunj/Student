6e04b3983d03217fbe1faf36c4464fee0699b590
Yun Jang <jang.yunj7@gmail.com>
2023-02-01 23:37:35 -0500

    updated table

--- student.sql
+++ student.sql
@@ -15,6 +15,19 @@ CREATE TABLE student(
 SELECT * FROM student;
 INSERT INTO student VALUES(1, 'Jack', 'Biology');
 INSERT INTO student VALUES(2, 'Kate', 'Sociology');
-
 INSERT INTO student(student_id, name) VALUES(3, 'Claire');
+
+UPDATE student
+SET major = 'Comp Sci'
+WHERE student_id = 3;
+
+UPDATE student
+SET major = 'Biochemistry'
+WHERE major = 'Bio' OR major = 'Chemistry'
+
+UPDATE student
+SET name = 'Tom', major = 'undecided'
+WHERE student_id = 1;
+
+DELETE FROM student;
 
